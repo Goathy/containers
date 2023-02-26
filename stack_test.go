aa34538293c5be992991d76c7e902ee68ac64b9a
Jakub Gałecki <65628898+Goathy@users.noreply.github.com>
2023-02-26 20:30:09 +0100

    test(stack): mix value types in test cases (#7)

--- stack_test.go
+++ stack_test.go
@@ -18,13 +18,13 @@ func TestStack(t *testing.T) {
 	})
 
 	t.Run("should create stack without size limit", func(t *testing.T) {
-		stack, err := stack.New[int64](-1)
+		stack, err := stack.New[float64](-1)
 
 		assertBool(t, err != nil, "unexpected error")
 
 		assertBool(t, !stack.IsEmpty(), "empty stack should be empty")
 
-		for _, el := range []int64{1, 2, 3, 4, 5, 6, 7} {
+		for _, el := range []float64{1, 0.2, 00.3, 4.01, 5.4, 345.6, 7} {
 			stack.Push(el)
 		}
 
@@ -32,7 +32,7 @@ func TestStack(t *testing.T) {
 	})
 
 	t.Run("should not create stack with size less then -1", func(t *testing.T) {
-		stack, err := stack.New[int64](-2)
+		stack, err := stack.New[string](-2)
 
 		want := errors.New("negative stack size")
 
@@ -42,13 +42,13 @@ func TestStack(t *testing.T) {
 	})
 
 	t.Run("stack should be full", func(t *testing.T) {
-		stack, err := stack.New[int64](5)
+		stack, err := stack.New[rune](5)
 
 		assertBool(t, err != nil, "unexpected error")
 
 		assertBool(t, !stack.IsEmpty(), "stack should be empty")
 
-		for _, el := range []int64{1, 2, 3, 4, 5} {
+		for _, el := range []rune{'a', 'b', 'c', 'd', 'e'} {
 			stack.Push(el)
 		}
 
@@ -56,11 +56,11 @@ func TestStack(t *testing.T) {
 	})
 
 	t.Run("should return error if provide more values then stack limit", func(t *testing.T) {
-		stack, err := stack.New[int64](5)
+		stack, err := stack.New[int](5)
 
 		assertBool(t, err != nil, "unexpected error")
 
-		for _, el := range []int64{1, 2, 3, 4, 5} {
+		for _, el := range []int{1, 2, 3, 4, 5} {
 			err := stack.Push(el)
 
 			assertBool(t, err != nil, "unexpected error")
@@ -74,7 +74,7 @@ func TestStack(t *testing.T) {
 	})
 
 	t.Run("should return error if pop more values then stack store", func(t *testing.T) {
-		stack, err := stack.New[int64](0)
+		stack, err := stack.New[complex128](0)
 
 		assertBool(t, err != nil, "unexpected error")
 
@@ -85,17 +85,20 @@ func TestStack(t *testing.T) {
 		assertError(t, want, err)
 	})
 
-	t.Run("stack should pop values in reverso order to push", func(t *testing.T) {
-		stack, err := stack.New[int64](5)
+	t.Run("stack should pop values in revers order to push", func(t *testing.T) {
+		type TestStruct struct {
+			description string
+			value       int
+		}
+
+		elements := []TestStruct{{"foo", 10}, {"bar", 20}, {"baz", 30}}
+
+		stack, err := stack.New[TestStruct](3)
 
 		assertBool(t, err != nil, "unexpected error")
 
-		elements := []int64{1, 2, 3, 4, 5}
-
 		for _, el := range elements {
-			err := stack.Push(el)
-
-			assertBool(t, err != nil, "unexpected error")
+			stack.Push(el)
 		}
 
 		assertBool(t, !stack.IsFull(), "stack should be full")
@@ -105,60 +108,9 @@ func TestStack(t *testing.T) {
 
 			assertBool(t, err != nil, "unexpected error")
 
-			assertValues(t, el, elements[i])
+			assertValue(t, el, elements[i])
 		}
-
-		assertBool(t, !stack.IsEmpty(), "stackshould be empty")
 	})
-}
-
-func TestStackWithStrings(t *testing.T) {
-	elements := []string{"foo", "bar", "baz"}
-
-	stack, err := stack.New[string](3)
-
-	assertBool(t, err != nil, "unexpected error")
-
-	for _, el := range elements {
-		stack.Push(el)
-	}
-
-	assertBool(t, !stack.IsFull(), "stack should be full")
-
-	for i := len(elements) - 1; i >= 0; i-- {
-		el, err := stack.Pop()
-
-		assertBool(t, err != nil, "unexpected error")
-
-		assertValues(t, el, elements[i])
-	}
-}
-
-func TestStackWithStruct(t *testing.T) {
-	type TestStruct struct {
-		description string
-		value       int
-	}
-
-	elements := []TestStruct{{"foo", 10}, {"bar", 20}, {"baz", 30}}
-
-	stack, err := stack.New[TestStruct](3)
-
-	assertBool(t, err != nil, "unexpected error")
-
-	for _, el := range elements {
-		stack.Push(el)
-	}
-
-	assertBool(t, !stack.IsFull(), "stack should be full")
-
-	for i := len(elements) - 1; i >= 0; i-- {
-		el, err := stack.Pop()
-
-		assertBool(t, err != nil, "unexpected error")
-
-		assertValues(t, el, elements[i])
-	}
 }
 
 func assertError(t testing.TB, want, got error) {
@@ -176,7 +128,7 @@ func assertBool(t testing.TB, got bool, msg string) {
 	}
 }
 
-func assertValues(t testing.TB, got any, want any) {
+func assertValue(t testing.TB, got any, want any) {
 	t.Helper()
 
 	if !reflect.DeepEqual(got, want) {
