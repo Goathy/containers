8258aae6dc45ef105afcc35136cc99a54fc731a9
Jakub Gałecki <goathy.dev@gmail.com>
2023-03-08 20:57:26 +0000

    test(queue): refactor tests

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -8,77 +8,74 @@ import (
 	"github.com/Goathy/containers/queue"
 )
 
-func TestQueue(t *testing.T) {
-	t.Run("should create stack instance", func(t *testing.T) {
-		q := queue.New[any]()
+func TestQueueConstructor(t *testing.T) {
+	q := queue.New[any]()
 
-		assertBool(t, q == nil, "queue should not be nil")
-	})
+	assertBool(t, q == nil, "queue should not be nil")
+}
 
-	t.Run("should insert items into queue", func(t *testing.T) {
-		q := queue.New[int]()
+func TestEnqueue(t *testing.T) {
+	q := queue.New[int]()
 
-		input := []int{1, 2, 3, 4, 5}
+	input := []int{1, 2, 3, 4, 5}
 
-		for _, in := range input {
-			q.Enqueue(in)
-		}
-	})
+	for _, in := range input {
+		q.Enqueue(in)
+	}
+}
 
-	t.Run("should remove items from queue", func(t *testing.T) {
-		q := queue.New[string]()
+func TestDequeue(t *testing.T) {
+	q := queue.New[string]()
 
-		input := []string{"a", "b", "c", "d", "e"}
+	input := []string{"a", "b", "c", "d", "e"}
 
-		for _, in := range input {
-			q.Enqueue(in)
-		}
+	for _, in := range input {
+		q.Enqueue(in)
+	}
 
-		got := make([]string, 0)
+	got := make([]string, 0)
 
-		for !q.IsEmpty() {
-			v := q.Dequeue()
+	for !q.IsEmpty() {
+		v := q.Dequeue()
 
-			got = append(got, v)
-		}
+		got = append(got, v)
+	}
 
-		assertBool(t, !reflect.DeepEqual(got, input), fmt.Sprintf("want %q, got %q", input, got))
-	})
+	assertBool(t, !reflect.DeepEqual(got, input), fmt.Sprintf("want %q, got %q", input, got))
+}
 
-	t.Run("should always peek first element from queue", func(t *testing.T) {
+func TestPeek(t *testing.T) {
+	tt := []struct {
+		desc  string
+		input []int
+		want  int
+	}{
+		{
+			desc:  "should pick 1",
+			input: []int{1, 2, 3, 4, 5},
+			want:  1,
+		},
+		{
+			desc:  "should pick 10",
+			input: []int{10},
+			want:  10,
+		},
+	}
 
-		tt := []struct {
-			desc  string
-			input []int
-			want  int
-		}{
-			{
-				desc:  "should pick 1",
-				input: []int{1, 2, 3, 4, 5},
-				want:  1,
-			},
-			{
-				desc:  "should pick 10",
-				input: []int{10},
-				want:  10,
-			},
-		}
+	for _, tc := range tt {
+		t.Run(tc.desc, func(t *testing.T) {
+			q := queue.New[int]()
 
-		for _, tc := range tt {
-			t.Run(tc.desc, func(t *testing.T) {
-				q := queue.New[int]()
+			for _, in := range tc.input {
+				q.Enqueue(in)
+			}
 
-				for _, in := range tc.input {
-					q.Enqueue(in)
-				}
+			got := q.Peek()
 
-				got := q.Peek()
+			assertValue(t, got, tc.want)
+		})
+	}
 
-				assertValue(t, got, tc.want)
-			})
-		}
-
-	})
 }
 
 func TestIsEmpty(t *testing.T) {
