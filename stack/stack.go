23aa276e83d1ea697338e450caaddb2011547788
Jakub Gałecki <goathy.dev@gmail.com>
2023-03-08 21:21:17 +0000

    refactor(stack): refactor stack
    * Remove `size` from stack.

--- stack/stack.go
+++ stack/stack.go
@@ -1,80 +1,38 @@
 package stack
 
-import (
-	"errors"
-)
-
-var (
-	ErrorOverflow = errors.New("stack overflow")
-	// EOS error is returned when Pop() or Peek() method are called
-	// on empty stack
-	EOS             = errors.New("EOS")
-	ErrNegativeSize = errors.New("negative stack size")
-)
-
 type node[V any] struct {
 	value V
 	prev  *node[V]
 }
 
 type stack[V any] struct {
-	size   int64
-	length int64
-	top    *node[V]
+	top *node[V]
 }
 
-func New[V any](size int64) (*stack[V], error) {
-	if size == -1 || size >= 0 {
-		return &stack[V]{size: size, length: 0, top: nil}, nil
-	}
-	return nil, ErrNegativeSize
-}
-
-func (s *stack[V]) IsFull() bool {
-	if s.size == -1 {
-		return false
-	}
-	return s.length == s.size
+func New[V any]() *stack[V] {
+	return &stack[V]{top: nil}
 }
 
 func (s *stack[V]) IsEmpty() bool {
 	return s.top == nil
 }
 
-func (s *stack[V]) Push(v V) error {
-	if s.IsFull() {
-		return ErrorOverflow
-	}
-
-	if s.IsEmpty() {
+func (s *stack[V]) Push(v V) {
+	if s.top == nil {
 		s.top = &node[V]{value: v, prev: nil}
-		s.length++
-		return nil
+		return
 	}
 
 	s.top = &node[V]{value: v, prev: s.top}
-	s.length++
-
-	return nil
 }
 
-func (s *stack[V]) Pop() (V, error) {
-	if s.IsEmpty() {
-		// *new(V) create zero value of provided type, equivalent to e.g. var zero Value
-		return *new(V), EOS
-	}
+func (s *stack[V]) Pop() V {
+	value := s.top.value
+	s.top = s.top.prev
 
-	n := s.top
-	s.top = n.prev
-	s.length--
-
-	return n.value, nil
+	return value
 }
 
-func (s *stack[V]) Peek() (V, error) {
-	if s.IsEmpty() {
-		return *new(V), EOS
-	}
-
-	return s.top.value, nil
+func (s *stack[V]) Peek() V {
+	return s.top.value
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,151 +1,109 @@
 package stack_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/Goathy/containers/stack"
 )
 
-func TestStack(t *testing.T) {
-	t.Run("should create stack instance", func(t *testing.T) {
-		s, err := stack.New[int64](0)
+func TestStackConstructor(t *testing.T) {
+	s := stack.New[any]()
 
-		assertBool(t, err != nil, "unexpected error")
-
-		assertBool(t, s == nil, "stack should not be nil")
-	})
-
-	t.Run("should create stack without size limit", func(t *testing.T) {
-		s, err := stack.New[float64](-1)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		assertBool(t, !s.IsEmpty(), "empty stack should be empty")
-
-		for _, el := range []float64{1, 0.2, 00.3, 4.01, 5.4, 345.6, 7} {
-			s.Push(el)
-		}
-
-		assertBool(t, s.IsFull(), "stack with no size limit should not be full")
-	})
-
-	t.Run("should not create stack with size less then -1", func(t *testing.T) {
-		s, err := stack.New[string](-2)
-
-		assertError(t, stack.ErrNegativeSize, err)
-
-		assertBool(t, s != nil, "stack should be nil")
-	})
-
-	t.Run("stack should be full", func(t *testing.T) {
-		s, err := stack.New[rune](5)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		assertBool(t, !s.IsEmpty(), "stack should be empty")
-
-		for _, el := range []rune{'a', 'b', 'c', 'd', 'e'} {
-			s.Push(el)
-		}
-
-		assertBool(t, !s.IsFull(), "stack should be full")
-	})
-
-	t.Run("should return error if provide more values then stack limit", func(t *testing.T) {
-		s, err := stack.New[int](5)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		for _, el := range []int{1, 2, 3, 4, 5} {
-			err := s.Push(el)
-
-			assertBool(t, err != nil, "unexpected error")
-		}
-
-		err = s.Push(6)
-
-		assertError(t, stack.ErrorOverflow, err)
-	})
-
-	t.Run("should return error if pop more values then stack store", func(t *testing.T) {
-		s, err := stack.New[complex128](0)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		_, err = s.Pop()
-
-		assertError(t, stack.EOS, err)
-	})
-
-	t.Run("stack should pop values in revers order to push", func(t *testing.T) {
-		type TestStruct struct {
-			description string
-			value       int
-		}
-
-		elements := []TestStruct{{"foo", 10}, {"bar", 20}, {"baz", 30}}
-
-		s, err := stack.New[TestStruct](3)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		for _, el := range elements {
-			s.Push(el)
-		}
-
-		assertBool(t, !s.IsFull(), "stack should be full")
-
-		for i := len(elements) - 1; i >= 0; i-- {
-			el, err := s.Pop()
-
-			assertBool(t, err != nil, "unexpected error")
-
-			assertValue(t, el, elements[i])
-		}
-	})
-
-	t.Run("peek should display value without popping value from stack", func(t *testing.T) {
-		s, err := stack.New[float32](1)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		s.Push(6.32)
-
-		assertBool(t, !s.IsFull(), "stack should be full")
-
-		got, err := s.Peek()
-
-		want := float32(6.32)
-
-		assertValue(t, got, want)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		assertBool(t, !s.IsFull(), "stack should be full")
-
-	})
-
-	t.Run("peek on empty stack should return error", func(t *testing.T) {
-		s, err := stack.New[float32](0)
-
-		assertBool(t, err != nil, "unexpected error")
-
-		assertBool(t, !s.IsFull(), "stack should be full")
-
-		_, err = s.Peek()
-
-		assertError(t, stack.EOS, err)
-
-		assertBool(t, !s.IsFull(), "stack should be full")
-	})
+	assertBool(t, s == nil, "stack should not be nil")
 }
 
-func assertError(t testing.TB, want, got error) {
-	t.Helper()
+func TestPush(t *testing.T) {
+	s := stack.New[int]()
 
-	if want.Error() != got.Error() {
-		t.Errorf("want %q, got %q", want, got)
+	input := []int{1, 2, 3, 4, 5}
+
+	for _, in := range input {
+		s.Push(in)
+	}
+}
+
+func TestPop(t *testing.T) {
+	s := stack.New[string]()
+
+	for _, v := range []string{"a", "b", "c", "d", "e"} {
+		s.Push(v)
+	}
+
+	want := []string{"e", "d", "c", "b", "a"}
+	got := make([]string, 0)
+
+	for !s.IsEmpty() {
+		v := s.Pop()
+
+		got = append(got, v)
+	}
+
+	assertBool(t, !reflect.DeepEqual(got, want), fmt.Sprintf("want %q, got %q", want, got))
+}
+
+func TestPeek(t *testing.T) {
+	tt := []struct {
+		desc  string
+		input []int
+		want  int
+	}{
+		{
+			desc:  "should pick 5",
+			input: []int{1, 2, 3, 4, 5},
+			want:  5,
+		},
+		{
+			desc:  "should pick 10",
+			input: []int{10},
+			want:  10,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.desc, func(t *testing.T) {
+			s := stack.New[int]()
+
+			for _, in := range tc.input {
+				s.Push(in)
+			}
+
+			got := s.Peek()
+
+			assertValue(t, got, tc.want)
+		})
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	tt := []struct {
+		desc  string
+		input []string
+		want  bool
+	}{
+		{
+			desc:  "stack should be empty",
+			input: []string{},
+			want:  true,
+		},
+		{
+			desc:  "stack should not be empty",
+			input: []string{"a", "b"},
+			want:  false,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.desc, func(t *testing.T) {
+			s := stack.New[string]()
+
+			for _, in := range tc.input {
+				s.Push(in)
+			}
+
+			assertBool(t, s.IsEmpty() != tc.want, fmt.Sprintf("want %t, got %t", tc.want, s.IsEmpty()))
+		})
 	}
 }
 
